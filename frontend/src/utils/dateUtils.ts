0cf67e70e90dd351bc23a6308e6e01a568495ee1
Manuel Gonzalez <porcelanic0907@gmail.com>
2025-06-16 22:40:11 -0500

    [FIX] the date formating was done incorrectly before, it's now fixed

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -27,6 +27,6 @@ export const formatDateForInput = (
 
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
+    const day = (date.getDate()+1).toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
 };
